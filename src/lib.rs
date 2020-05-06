123ca2e6f4707556c6e58b8e8953a84815f7d96b
Akshay Narayan <akshayn@mit.edu>
2020-05-05 22:01:26 -0400

    SpinTicker: ticker that busy-spins on yield()

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,4 @@
 //! Exponentially distributed timer for your Poisson-arrivals needs.
-//!
-//! # Example
-//! ```rust
-//! #[tokio::main]
-//! async fn main() {
-//!     let mut t = poisson_ticker::Ticker::new(std::time::Duration::from_millis(10));
-//!     let now = std::time::Instant::now();
-//!     for _ in 0usize..5 {
-//!         (&mut t).await;
-//!     }
-//!     println!("elapsed: {:?}", now.elapsed());
-//! }
-//! ```
 
 use async_timer::oneshot::{Oneshot, Timer};
 use core::task::{Context, Poll};
@@ -20,7 +7,132 @@ use rand_distr::{Distribution, Exp};
 use std::future::Future;
 use std::pin::Pin;
 use std::time::Duration;
+use std::time::Instant;
 
+/// Calls `tokio::task::yield_now()` in a loop for each tick.
+///
+/// # Example
+/// ```rust
+/// # #[tokio::main]
+/// # async fn main() {
+/// let mut t = poisson_ticker::SpinTicker::new(std::time::Duration::from_millis(10));
+/// let now = std::time::Instant::now();
+/// for _ in 0usize..5 {
+///     (&mut t).await;
+/// }
+/// println!("elapsed: {:?}", now.elapsed());
+/// # }
+/// ```
+pub struct SpinTicker(SpinTimer, Option<Pin<Box<dyn Future<Output = ()>>>>);
+
+impl Future for SpinTicker {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        if let None = self.1 {
+            self.1 = Some(Box::pin(self.0.wait()));
+        }
+
+        futures_util::ready!(self.1.as_mut().unwrap().as_mut().poll(cx));
+        self.1 = None;
+        Poll::Ready(())
+    }
+}
+
+impl Stream for SpinTicker {
+    type Item = ();
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        self.poll(cx).map(Some)
+    }
+}
+
+impl SpinTicker {
+    pub fn new(d: Duration) -> Self {
+        Self(SpinTimer::new(d), None)
+    }
+
+    pub fn new_with_log_id(d: Duration, id: usize) -> Self {
+        Self(SpinTimer::new_with_log_id(d, id), None)
+    }
+}
+
+struct SpinTimer {
+    distr: Exp<f64>,
+    mean_ns: u64,
+    id: Option<usize>,
+}
+
+impl SpinTimer {
+    fn new(d: Duration) -> Self {
+        let mean_ns = d.as_nanos() as _;
+        let lambda = 1. / d.as_nanos() as f64;
+        let r = Exp::new(lambda).expect("Make exponential distr");
+        Self {
+            distr: r,
+            mean_ns,
+            id: None,
+        }
+    }
+
+    fn new_with_log_id(d: Duration, id: usize) -> Self {
+        let mean_ns = d.as_nanos() as _;
+        let lambda = 1. / d.as_nanos() as f64;
+        let r = Exp::new(lambda).expect("Make exponential distr");
+        Self {
+            distr: r,
+            mean_ns,
+            id: Some(id),
+        }
+    }
+
+    fn wait(&mut self) -> Pin<Box<dyn Future<Output = ()> + 'static>> {
+        let mut rng = rand::thread_rng();
+        let mut next_interarrival_ns = self.distr.sample(&mut rng) as u64;
+        if next_interarrival_ns > self.mean_ns * 10 {
+            tracing::warn!(
+                id = ?self.id,
+                sampled_wait_ns = ?next_interarrival_ns,
+                mean_ns = ?self.mean_ns,
+                "suspicious wait"
+            );
+        }
+
+        if next_interarrival_ns < 100 {
+            tracing::debug!(
+                id = ?self.id,
+                sampled_wait_ns = ?next_interarrival_ns,
+                mean_ns = ?self.mean_ns,
+                "wait too short"
+            );
+
+            next_interarrival_ns = 100;
+        }
+
+        let next_dur = Duration::from_nanos(next_interarrival_ns);
+        let next_time = Instant::now() + next_dur;
+        Box::pin(async move {
+            while Instant::now() < next_time {
+                tokio::task::yield_now().await;
+            }
+        })
+    }
+}
+
+/// Naive Ticker which calls a timer, no adjustment.
+///
+/// # Example
+/// ```rust
+/// # #[tokio::main]
+/// # async fn main() {
+/// let mut t = poisson_ticker::Ticker::new(std::time::Duration::from_millis(10));
+/// let now = std::time::Instant::now();
+/// for _ in 0usize..5 {
+///     (&mut t).await;
+/// }
+/// println!("elapsed: {:?}", now.elapsed());
+/// # }
+/// ```
 pub struct Ticker {
     should_restart: bool,
     curr_timer: Timer,
@@ -117,21 +229,5 @@ impl Stream for Ticker {
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
         self.poll(cx).map(Some)
-    }
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn reuse() {
-        let mut rt = tokio::runtime::Runtime::new().unwrap();
-        rt.block_on(async move {
-            let mut t = super::Ticker::new(std::time::Duration::from_millis(10));
-            let now = std::time::Instant::now();
-            for _ in 0usize..5 {
-                (&mut t).await;
-            }
-            println!("elapsed: {:?}", now.elapsed());
-        });
     }
 }
