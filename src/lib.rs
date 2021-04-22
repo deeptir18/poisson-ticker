a033738ed41d3b3fd9c789325c65301369d5dc68
Akshay Narayan <akshayn@mit.edu>
2021-04-22 19:18:43 +0000

    count deficit

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,13 @@ use futures_util::stream::Stream;
 use rand_distr::{Distribution, Exp};
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::{
+    atomic::{AtomicU64, Ordering},
+    Arc,
+};
 use std::time::Duration;
 use std::time::Instant;
+use tracing::trace;
 
 /// Calls `tokio::task::yield_now()` in a loop for each tick.
 ///
@@ -14,12 +19,19 @@ use std::time::Instant;
 /// ```rust
 /// # #[tokio::main]
 /// # async fn main() {
-/// let mut t = poisson_ticker::SpinTicker::new(std::time::Duration::from_millis(10));
+/// # use tracing_subscriber::prelude::*; use tracing::info;
+/// # let subscriber =
+/// tracing_subscriber::fmt().with_test_writer().with_max_level(tracing_subscriber::filter::LevelFilter::TRACE).finish().set_default();
+/// let mut t = poisson_ticker::SpinTicker::new(std::time::Duration::from_micros(200));
 /// let now = std::time::Instant::now();
-/// for _ in 0usize..5 {
+/// info!(?now, "start");
+/// for _ in 0usize..250 {
 ///     (&mut t).await;
 /// }
-/// assert!(now.elapsed() > std::time::Duration::from_millis(50));
+/// let el = now.elapsed();
+/// info!(?el, "end");
+/// assert!(el > std::time::Duration::from_millis(40));
+/// assert!(el < std::time::Duration::from_millis(60));
 /// # }
 /// ```
 pub struct SpinTicker(
@@ -61,67 +73,58 @@ impl SpinTicker {
 
 struct SpinTimer {
     distr: Exp<f64>,
-    mean_ns: u64,
+    deficit_ns: Arc<AtomicU64>,
     id: Option<usize>,
 }
 
 impl SpinTimer {
     fn new(d: Duration) -> Self {
-        let mean_ns = d.as_nanos() as _;
-        let lambda = 1. / d.as_nanos() as f64;
-        let r = Exp::new(lambda).expect("Make exponential distr");
-        Self {
-            distr: r,
-            mean_ns,
-            id: None,
-        }
+        Self::new_with_log_id(d, None)
     }
 
-    fn new_with_log_id(d: Duration, id: usize) -> Self {
-        let mean_ns = d.as_nanos() as _;
+    fn new_with_log_id(d: Duration, id: impl Into<Option<usize>>) -> Self {
         let lambda = 1. / d.as_nanos() as f64;
         let r = Exp::new(lambda).expect("Make exponential distr");
         Self {
             distr: r,
-            mean_ns,
-            id: Some(id),
+            deficit_ns: Default::default(),
+            id: id.into(),
         }
     }
 
     fn wait(&mut self) -> Pin<Box<dyn Future<Output = ()> + Send + 'static>> {
+        let start = Instant::now();
         let mut rng = rand::thread_rng();
         let next_interarrival_ns = self.distr.sample(&mut rng) as u64;
-        if next_interarrival_ns < 100 {
-            tracing::trace!(
-                id = ?self.id,
-                sampled_wait_ns = ?next_interarrival_ns,
-                mean_ns = ?self.mean_ns,
-                "wait too short"
-            );
-
-            // have a min wait or return immediately?
-            //next_interarrival_ns = 100;
+        if self.deficit_ns.load(Ordering::Acquire) > next_interarrival_ns {
+            // load doesn't matter, since we don't care about the read
+            let deficit = self
+                .deficit_ns
+                .fetch_sub(next_interarrival_ns, Ordering::Release);
+            trace!(?deficit, "returning immediately from deficit");
             return Box::pin(futures_util::future::ready(()));
         }
 
         let next_dur = Duration::from_nanos(next_interarrival_ns);
-        let start = Instant::now();
         let next_time = start + next_dur;
         let id = self.id;
+        let deficit_ns = Arc::clone(&self.deficit_ns);
         Box::pin(async move {
             while Instant::now() < next_time {
                 tokio::task::yield_now().await;
             }
 
             let elapsed = start.elapsed();
-            if elapsed.as_nanos() > next_interarrival_ns as u128 * 10 {
-                tracing::trace!(
-                    ?id,
-                    ?elapsed,
-                    sampled_wait_ns = ?next_interarrival_ns,
-                    "suspicious wait"
-                );
-            }
+            let elapsed_ns = elapsed.as_nanos() as u64;
+            let deficit =
+                deficit_ns.fetch_add(elapsed_ns - next_interarrival_ns, Ordering::Release);
+            trace!(
+                ?id,
+                ?elapsed,
+                ?deficit,
+                sampled_wait_ns = ?next_interarrival_ns,
+                "waited"
+            );
         })
     }
 }
