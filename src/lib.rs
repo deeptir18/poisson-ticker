c05a42d804605a35b874955938cb4d6059647b07
Akshay Narayan <akshayn@mit.edu>
2021-08-25 19:24:45 +0000

    const mode

--- src/lib.rs
+++ src/lib.rs
@@ -20,26 +20,26 @@ use tracing::trace;
 /// # #[tokio::main]
 /// # async fn main() {
 /// # use tracing_subscriber::prelude::*; use tracing::info;
-/// # let subscriber =
-/// tracing_subscriber::fmt().with_test_writer().with_max_level(tracing_subscriber::filter::LevelFilter::TRACE).finish().set_default();
+/// # let subscriber = tracing_subscriber::fmt().with_test_writer()
+/// #    .with_max_level(tracing_subscriber::filter::LevelFilter::TRACE).finish().set_default();
 /// let mut t = poisson_ticker::SpinTicker::new(std::time::Duration::from_micros(200));
 /// let now = std::time::Instant::now();
-/// info!(?now, "start");
+/// # info!(?now, "start");
 /// for _ in 0usize..250 {
 ///     (&mut t).await;
 /// }
 /// let el = now.elapsed();
-/// info!(?el, "end");
+/// # info!(?el, "end");
 /// assert!(el > std::time::Duration::from_millis(40));
 /// assert!(el < std::time::Duration::from_millis(60));
 /// # }
 /// ```
-pub struct SpinTicker(
-    SpinTimer,
+pub struct SpinTicker<T>(
+    T,
     Option<Pin<Box<dyn Future<Output = ()> + Send + 'static>>>,
 );
 
-impl Future for SpinTicker {
+impl<T: Timer + Unpin> Future for SpinTicker<T> {
     type Output = ();
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
@@ -53,7 +53,7 @@ impl Future for SpinTicker {
     }
 }
 
-impl Stream for SpinTicker {
+impl<T: Timer + Unpin> Stream for SpinTicker<T> {
     type Item = ();
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
@@ -61,17 +61,29 @@ impl Stream for SpinTicker {
     }
 }
 
-impl SpinTicker {
-    pub fn new(d: Duration) -> Self {
-        Self(SpinTimer::new(d), None)
+impl SpinTicker<()> {
+    pub fn new_poisson(d: Duration) -> SpinTicker<SpinTimer> {
+        SpinTicker(SpinTimer::new(d), None)
     }
 
-    pub fn new_with_log_id(d: Duration, id: usize) -> Self {
-        Self(SpinTimer::new_with_log_id(d, id), None)
+    pub fn new_poisson_with_log_id(d: Duration, id: usize) -> SpinTicker<SpinTimer> {
+        SpinTicker(SpinTimer::new_with_log_id(d, id), None)
+    }
+
+    pub fn new_const(d: Duration) -> SpinTicker<ConstTimer> {
+        SpinTicker(ConstTimer::new(d), None)
+    }
+
+    pub fn new_const_with_log_id(d: Duration, id: usize) -> SpinTicker<ConstTimer> {
+        SpinTicker(ConstTimer::new_with_log_id(d, id), None)
     }
 }
 
-struct SpinTimer {
+pub trait Timer {
+    fn wait(&mut self) -> Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
+}
+
+pub struct SpinTimer {
     distr: Exp<f64>,
     deficit_ns: Arc<AtomicU64>,
     id: Option<usize>,
@@ -91,7 +103,9 @@ impl SpinTimer {
             id: id.into(),
         }
     }
+}
 
+impl Timer for SpinTimer {
     fn wait(&mut self) -> Pin<Box<dyn Future<Output = ()> + Send + 'static>> {
         let start = Instant::now();
         let mut rng = rand::thread_rng();
@@ -123,6 +137,60 @@ impl SpinTimer {
                 ?elapsed,
                 ?deficit,
                 sampled_wait_ns = ?next_interarrival_ns,
+                "waited"
+            );
+        })
+    }
+}
+
+pub struct ConstTimer {
+    dur_nanos: usize,
+    deficit_ns: Arc<AtomicU64>,
+    id: Option<usize>,
+}
+
+impl ConstTimer {
+    fn new(d: Duration) -> Self {
+        Self::new_with_log_id(d, None)
+    }
+
+    fn new_with_log_id(d: Duration, id: impl Into<Option<usize>>) -> Self {
+        Self {
+            dur_nanos: d.as_nanos() as _,
+            deficit_ns: Default::default(),
+            id: id.into(),
+        }
+    }
+}
+
+impl Timer for ConstTimer {
+    fn wait(&mut self) -> Pin<Box<dyn Future<Output = ()> + Send + 'static>> {
+        let start = Instant::now();
+        let dur = self.dur_nanos as u64;
+        if self.deficit_ns.load(Ordering::Acquire) > dur {
+            // load doesn't matter, since we don't care about the read
+            let deficit = self.deficit_ns.fetch_sub(dur, Ordering::Release);
+            trace!(?deficit, "returning immediately from deficit");
+            return Box::pin(futures_util::future::ready(()));
+        }
+
+        let next_dur = Duration::from_nanos(dur);
+        let next_time = start + next_dur;
+        let id = self.id;
+        let deficit_ns = Arc::clone(&self.deficit_ns);
+        Box::pin(async move {
+            while Instant::now() < next_time {
+                tokio::task::yield_now().await;
+            }
+
+            let elapsed = start.elapsed();
+            let elapsed_ns = elapsed.as_nanos() as u64;
+            let deficit = deficit_ns.fetch_add(elapsed_ns - dur, Ordering::Release);
+            trace!(
+                ?id,
+                ?elapsed,
+                ?deficit,
+                sampled_wait_ns = ?dur,
                 "waited"
             );
         })
