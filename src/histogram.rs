f587be2b9f6144dc778de59f730e8d4c58eaf806
Deepti Raghavan <deeptir@brown.edu>
2024-11-16 18:42:27 -0500

    Change to calculate tput and latency within a window

--- src/histogram.rs
+++ src/histogram.rs
@@ -1,6 +1,220 @@
-use color_eyre::eyre::{bail, ensure, Result};
+use color_eyre::eyre::{bail, Result};
 use std::fs::File;
 use std::io::Write;
+use std::time::{Duration, Instant};
+
+#[derive(Debug, PartialEq, Clone, Default)]
+pub struct LatencyMap {
+    // map from request ID to (start time, end time)
+    // end time is None if request was dropped before exp. ended
+    map: std::collections::BTreeMap<usize, (Instant, Option<Instant>)>,
+}
+
+impl LatencyMap {
+    pub fn new() -> Self {
+        LatencyMap {
+            map: std::collections::BTreeMap::default(),
+        }
+    }
+
+    pub fn from_sent_and_recv_times(
+        sent_times: &std::collections::HashMap<usize, Instant>,
+        recv_times: &std::collections::HashMap<usize, Instant>,
+    ) -> Result<Self> {
+        let mut map = std::collections::BTreeMap::new();
+        for (id, sent_time) in sent_times.iter() {
+            if let Some(recv_time) = recv_times.get(id) {
+                map.insert(*id, (*sent_time, Some(*recv_time)));
+            } else {
+                map.insert(*id, (*sent_time, None));
+            }
+        }
+        Ok(LatencyMap { map })
+    }
+
+    pub fn record(
+        &mut self,
+        request_id: usize,
+        start: Instant,
+        end: Option<Instant>,
+    ) -> Result<()> {
+        if let Some(end_time) = end {
+            if end_time.checked_duration_since(start).is_none() {
+                bail!(
+                    "End time is before start time: id {}, start {:?}, end {:?}",
+                    request_id,
+                    start,
+                    end_time
+                );
+            }
+        }
+        self.map.insert(request_id, (start, end));
+        Ok(())
+    }
+
+    pub fn get(&self, request_id: usize) -> Option<&(Instant, Option<Instant>)> {
+        self.map.get(&request_id)
+    }
+
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
+
+    pub fn dump(&self, msg: &str) {
+        tracing::info!(msg, len = self.len());
+    }
+
+    pub fn log_to_file(&self, path: &str) -> Result<()> {
+        let mut file = File::create(path)?;
+        for (request_id, (start, recvd_time)) in self.map.iter() {
+            if let Some(end) = recvd_time {
+                let latency = end.checked_duration_since(*start);
+                if latency.is_none() {
+                    bail!(
+                        "End time is before start time: id {}, start {:?}, end {:?}",
+                        request_id,
+                        start,
+                        end
+                    );
+                }
+                writeln!(file, "{},{:?}", request_id, latency.unwrap().as_secs_f64())?;
+            } else {
+                writeln!(file, "{}, DROPPED", request_id)?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn histogram_from_id_range(
+        &self,
+        start_id: usize,
+        end_id: usize,
+        use_time_window: bool,
+    ) -> Result<(ManualHistogram, usize, usize, f64, f64)> {
+        // This function considers a specific ID range
+        // And returns histogram, number of requests sent, number of requests received, sent time, and receive time
+        // If use_time_window is true, function considers window of time between when start_id was
+        // sent and end_id was sent (so sent and receive times are the same)
+        // Otherwise, it considers time for all responses to arrive (so sent and receive time
+        // may be different)
+        // If drops are present, only use_time_window makes sense
+        // returns sent and received time in seconds
+
+        if start_id >= end_id {
+            bail!("start_id must be less than end_id");
+        }
+
+        if !self.map.contains_key(&start_id) {
+            bail!("start_id not found in map: {}", start_id);
+        }
+
+        if !self.map.contains_key(&end_id) {
+            bail!("end_id not found in map : {}", end_id);
+        }
+
+        let mut histogram = ManualHistogram::new((end_id - start_id) as usize);
+        let start_time = self.map.get(&start_id).unwrap().0;
+        let last_sent_time = self.map.get(&end_id).unwrap().0;
+
+        if last_sent_time.checked_duration_since(start_time).is_none() {
+            bail!(
+                "End time is before start time: start_id {}, end_id {}",
+                start_id,
+                end_id
+            );
+        }
+
+        if !use_time_window {
+            // calculate time to send and receive all the data
+
+            let mut max_end_time: Option<(Instant, Duration)> = None;
+            for id in start_id..end_id {
+                let entry = self.map.get(&id);
+                if let Some((send_time, recv_time)) = entry {
+                    if recv_time.is_none() {
+                        bail!(
+                            "ID has no recv time: {}; cannot use id-based window with drops",
+                            id
+                        );
+                    }
+                    // record for latency histogram
+                    let rtt = recv_time.unwrap().duration_since(*send_time);
+                    histogram.record(rtt.as_nanos() as u64);
+
+                    if recv_time
+                        .unwrap()
+                        .checked_duration_since(start_time)
+                        .is_none()
+                    {
+                        bail!(
+                            "For id {}, recv_time is before send time of first id: {:?}, {:?}",
+                            id,
+                            recv_time.unwrap(),
+                            start_time
+                        );
+                    }
+
+                    // update max end time
+                    if let Some((_cur_max_end, cur_max_time_since_start)) = max_end_time {
+                        if recv_time.unwrap().duration_since(start_time) > cur_max_time_since_start
+                        {
+                            max_end_time = Some((
+                                recv_time.unwrap(),
+                                recv_time.unwrap().duration_since(start_time),
+                            ));
+                        }
+                    } else {
+                        max_end_time = Some((
+                            recv_time.unwrap(),
+                            recv_time.unwrap().duration_since(start_time),
+                        ));
+                    }
+                } else {
+                    bail!("ID not found in map: {}", id);
+                }
+            }
+            // return histogram, num sent, num received, sent time, received time
+            // histogram only contains data received within the time between first ID was sent and
+            // last ID was sent
+            // TODO: does num_sent = 1 + num_received?
+            let num_sent = end_id - start_id;
+            let num_received = histogram.len();
+            if histogram.len() != (end_id - start_id) {
+                bail!(
+                    "Histogram length does not match expected: {}, {}",
+                    histogram.len(),
+                    end_id - start_id
+                );
+            }
+            let sent_time = last_sent_time.duration_since(start_time).as_secs_f64();
+            let received_time = max_end_time.unwrap().1.as_secs_f64();
+            return Ok((histogram, num_sent, num_received, sent_time, received_time));
+        } else {
+            let num_sent = end_id - start_id + 1;
+            let mut num_received = 0;
+            // calculate number of objects sent and received between these IDs
+            // consider time as between when start_id is sent and end_id is sent
+            for id in start_id..end_id {
+                let entry = self.map.get(&id);
+                if let Some((send_time, recv_time_option)) = entry {
+                    if let Some(recv_time) = recv_time_option {
+                        // if receive time is within the sent time, count it
+                        if !last_sent_time.checked_duration_since(*recv_time).is_none() {
+                            num_received += 1;
+                            let rtt = recv_time.duration_since(*send_time);
+                            histogram.record(rtt.as_nanos() as u64);
+                        }
+                    }
+                } else {
+                    bail!("ID not found in map: {}", id);
+                }
+            }
+            let sent_time = last_sent_time.duration_since(start_time).as_secs_f64();
+            let received_time = last_sent_time.duration_since(start_time).as_secs_f64();
+            return Ok((histogram, num_sent, num_received, sent_time, received_time));
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Default)]
 pub struct ManualHistogram {
@@ -36,17 +250,6 @@ impl ManualHistogram {
         self.current_count
     }
 
-    pub fn init(rate_pps: u64, total_time_sec: u64) -> Self {
-        // ten percent over
-        let num_values = ((rate_pps * total_time_sec) as f64 * 1.10) as usize;
-        ManualHistogram {
-            current_count: 0,
-            latencies: vec![0u64; num_values],
-            sorted_latencies: Vec::default(),
-            is_sorted: false,
-        }
-    }
-
     pub fn is_sorted(&self) -> bool {
         self.is_sorted
     }
@@ -61,27 +264,11 @@ impl ManualHistogram {
     }
 
     pub fn sort(&mut self) -> Result<()> {
-        self.sort_and_truncate(0)
-    }
-
-    pub fn sort_and_truncate(&mut self, start: usize) -> Result<()> {
-        if self.is_sorted {
-            return Ok(());
-        }
-        ensure!(
-            start < self.current_count,
-            format!(
-                "Cannot truncate entire array: start: {}, count: {}",
-                start, self.current_count
-            )
-        );
-
-        self.sorted_latencies = self.latencies.as_slice()[start..self.current_count].to_vec();
+        self.sorted_latencies = self.latencies.as_slice()[0..self.current_count].to_vec();
         self.sorted_latencies.sort();
         self.is_sorted = true;
         Ok(())
     }
-
     pub fn value_at_quantile(&self, quantile: f64) -> Result<u64> {
         if self.sorted_latencies.len() == 0 {
             bail!("Cannot run value_at_quantile until sort() has been called.");
--- src/summary_stats.rs
+++ src/summary_stats.rs
@@ -1,13 +1,17 @@
-use super::histogram::ManualHistogram;
-use color_eyre::eyre::Result;
+use super::histogram::{LatencyMap, ManualHistogram};
+use color_eyre::eyre::{bail, Result};
 use serde::{Deserialize, Serialize};
 use serde_json::to_writer;
 use std::{collections::BTreeMap, fs::File};
 
+// This takes a manual histogram and stores it with less precision.
+// Useful when rates are very high.
+// When precision is None, is a normal histogram.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct SummaryHistogram {
     // Precision in terms of nanoseconds
-    pub precision: u64,
+    // when precision is none, record all items.
+    pub precision: Option<u64>,
     // Map from latency marker to count
     pub map: BTreeMap<u64, u64>,
     // Count of total latencies
@@ -17,7 +21,7 @@ pub struct SummaryHistogram {
 impl Default for SummaryHistogram {
     fn default() -> Self {
         SummaryHistogram {
-            precision: 1000000,
+            precision: None,
             map: BTreeMap::default(),
             count: 0,
         }
@@ -25,7 +29,7 @@ impl Default for SummaryHistogram {
 }
 
 impl SummaryHistogram {
-    fn from_manual(precision: u64, manual_hist: &ManualHistogram) -> Result<Self> {
+    fn from_manual(precision: Option<u64>, manual_hist: &ManualHistogram) -> Result<Self> {
         let mut hist = Self::default();
         hist.precision = precision;
         for lat in manual_hist.latencies_vec().iter() {
@@ -35,32 +39,65 @@ impl SummaryHistogram {
     }
 
     pub fn record(&mut self, latency: u64) {
-        let divisor = latency / self.precision;
-        let bucket = (divisor + 1) * self.precision;
-        *self.map.entry(bucket).or_insert(0) += 1;
-        self.count += 1;
+        if let Some(precision) = self.precision {
+            let divisor = latency / precision;
+            let bucket = (divisor + 1) * precision;
+            *self.map.entry(bucket).or_insert(0) += 1;
+            self.count += 1;
+            return;
+        } else {
+            *self.map.entry(latency).or_insert(0) += 1;
+            self.count += 1;
+        }
     }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SummaryStats {
     pub histogram: SummaryHistogram,
-    pub total_objects: usize,
+    pub total_objects_sent: usize,
+    pub total_objects_recv: usize,
     pub send_time: f64,
     pub receive_time: f64,
 }
 
 impl SummaryStats {
     pub fn new(
-        manual_histogram: &ManualHistogram,
-        total_send_time: std::time::Duration,
-        total_recv_time: std::time::Duration,
+        rate_seconds: f64,
+        exp_time_seconds: usize,
+        latency_map: &LatencyMap,
+        warmup_time_seconds: usize,
+        cooldown_time_seconds: usize,
+        use_time_window: bool,
+        histogram_precision: Option<u64>,
     ) -> Result<Self> {
+        // check warmup and cooldown times are valid
+        if warmup_time_seconds > (exp_time_seconds - cooldown_time_seconds) {
+            bail!("Warmup time must be less than experiment time - cooldown time");
+        }
+
+        // calculate ID window using the warmup and cooldown times
+        let start_id = (rate_seconds * warmup_time_seconds as f64).ceil() as usize;
+        let mut end_id =
+            (rate_seconds * (exp_time_seconds - cooldown_time_seconds) as f64).floor() as usize;
+
+        // if cooldown is 0, possible that end_id is greater than length of map
+        if end_id > latency_map.len() {
+            end_id = latency_map.len();
+        }
+
+        // get histogram, total sent, total recv, sent time, receive time from latency map
+        let (histogram, total_sent, total_recv, send_time, recv_time) =
+            latency_map.histogram_from_id_range(start_id, end_id, use_time_window)?;
+
+        let summary_histogram = SummaryHistogram::from_manual(histogram_precision, &histogram)?;
+
         Ok(SummaryStats {
-            total_objects: manual_histogram.len(),
-            send_time: total_send_time.as_secs_f64(),
-            receive_time: total_recv_time.as_secs_f64(),
-            histogram: SummaryHistogram::from_manual(1_000_000, &manual_histogram)?,
+            histogram: summary_histogram,
+            total_objects_sent: total_sent,
+            total_objects_recv: total_recv,
+            send_time,
+            receive_time: recv_time,
         })
     }
 }
